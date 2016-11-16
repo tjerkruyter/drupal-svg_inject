5d4980d516fe4cb1a7303dc235365177982b732b
Tjerk Ruijter <tjerk@tamtam.nl>
2016-11-16 14:10:53 +0100

    init

---
+++ src/Twig/SvgInjectExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\svg_inject\Twig;
+
+
+/**
+ * Class SvgInjectExtension
+ * Print related nodes
+ * @package Drupal\svg_inject\Twig
+ */
+class SvgInjectExtension extends \Twig_Extension {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName() {
+        return 'svg_inject';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions() {
+        return [
+            new \Twig_SimpleFunction('svg_inject', [$this, 'svgInject'], [
+                'is_safe' => ['html'],
+            ]),
+        ];
+    }
+
+    public function svgInject($path) {
+      if(file_exists($path)) {
+          return file_get_contents($path);
+      }
+    }
+}
